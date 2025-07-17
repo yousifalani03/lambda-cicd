b875fb020f44ac869e78df290943604b5529cb07
Yousif Alani <yousifalani03@yahoo.com>
2025-07-16 21:56:14 -0700

    lambda change

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated lambda from vs code')
+        'body': json.dumps('Hello updated lambda from vs code final')
     }
